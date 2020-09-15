7f42fd2cb0c8d62d3bec03e1d1e1a6569918e034
Bud Manz <admin@manzwebdesigns.com>
2020-09-14 20:48:47 -0400

    Vulnerability Fix

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,8 +4,8 @@ $(function() {
 
     // in a real application, the user/password should never be hardcoded
     // but for the demo application it's very convenient to do so
-    if (!usernameEl.val() || 'jane_admin' === usernameEl.val()) {
-        usernameEl.val('jane_admin');
-        passwordEl.val('kitten');
-    }
+    // if (!usernameEl.val() || 'jane_admin' === usernameEl.val()) {
+    //     usernameEl.val('jane_admin');
+    //     passwordEl.val('kitten');
+    // }
 });
